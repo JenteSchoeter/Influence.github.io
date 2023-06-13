6d99c7387b63eda8931feaedc91ccf0cca8a800b
JenteSchoeter <118397419+JenteSchoeter@users.noreply.github.com>
2023-06-13 21:04:10 +0200

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,12 @@
+/* Image gallery */
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+
+
